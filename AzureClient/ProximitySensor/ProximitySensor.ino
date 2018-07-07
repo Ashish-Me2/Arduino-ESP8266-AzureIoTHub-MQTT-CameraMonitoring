7f717a0f99331af4ff5bdbe13259dac5114a64c5
Ashish Mathur <ashish.mathur@live.com>
2018-07-07 08:29:22 +0530

    Added PIR Sensor Project

---
+++ AzureClient/ProximitySensor/ProximitySensor.ino
@@ -0,0 +1,17 @@
+/*
+ Name:		ProximitySensor.ino
+ Created:	7/7/2018 8:26:48 AM
+ Author:	Ashish
+*/
+
+int PIRInput = 5;
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+	pinMode(LED_BUILTIN, OUTPUT);
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
